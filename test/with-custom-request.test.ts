9d9e6180c2ef701454e25dcbb8e3ae795a41e52d
Gregor Martynus <gregor@martynus.net>
2019-09-04 12:50:20 -0700

    test: use custom `@octokit/request` instance

---
+++ test/with-custom-request.test.ts
@@ -0,0 +1,82 @@
+import fetchMock from "fetch-mock";
+import { request } from "@octokit/request";
+
+import { withCustomRequest } from "../src";
+
+type RequestOptions = {
+  body: string;
+};
+
+import { Parameters } from "../src/types";
+
+describe("withCustomRequest()", () => {
+  it("README example", () => {
+    const myRequest = request.defaults({
+      headers: {
+        authorization: 'token secret123',
+        'user-agent': 'test'
+      }
+    })
+    const myGraphql = withCustomRequest(myRequest)
+
+    const mockData = {
+      repository: {
+        issues: {
+          edges: [
+            {
+              node: {
+                title: "Foo"
+              }
+            },
+            {
+              node: {
+                title: "Bar"
+              }
+            },
+            {
+              node: {
+                title: "Baz"
+              }
+            }
+          ]
+        }
+      }
+    };
+
+    return myGraphql(
+      `
+        {
+          repository(owner: "octokit", name: "graphql.js") {
+            issues(last: 3) {
+              edges {
+                node {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        headers: {
+          authorization: `token secret123`
+        },
+        request: {
+          fetch: fetchMock.sandbox().post(
+            "https://api.github.com/graphql",
+            { data: mockData },
+            {
+              headers: {
+                accept: "application/vnd.github.v3+json",
+                authorization: "token secret123",
+                "user-agent": 'test'
+              }
+            }
+          )
+        }
+      }
+    ).then(result => {
+      expect(result).toStrictEqual(mockData);
+    });
+  });
+});
