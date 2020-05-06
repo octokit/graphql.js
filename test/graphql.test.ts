c760d71ac8f981430aefbbb784b075622f2e8e9b
Júlio Lopes <julio.lopes@outsystems.com>
2020-05-06 10:25:01 +0100

    Added test to mediatype previews.

--- test/graphql.test.ts
+++ test/graphql.test.ts
@@ -1,86 +1,83 @@
 import fetchMock from "fetch-mock";
 import { getUserAgent } from "universal-user-agent";
+import * as OctokitTypes from "@octokit/types";
 
 import { graphql } from "../src";
 
 import { VERSION } from "../src/version";
 const userAgent = `octokit-graphql.js/${VERSION} ${getUserAgent()}`;
 
-type RequestOptions = {
-  body: string;
-};
-
 import { RequestParameters } from "../src/types";
 
 describe("graphql()", () => {
-  it("is a function", () => {
-    expect(graphql).toBeInstanceOf(Function);
-  });
+    it("is a function", () => {
+        expect(graphql).toBeInstanceOf(Function);
+    });
 
-  it("README simple query example", () => {
-    const mockData = {
-      repository: {
-        issues: {
-          edges: [
-            {
-              node: {
-                title: "Foo"
-              }
+    it("README simple query example", () => {
+        const mockData = {
+            repository: {
+                issues: {
+                    edges: [
+                        {
+                            node: {
+                                title: "Foo",
+                            },
+                        },
+                        {
+                            node: {
+                                title: "Bar",
+                            },
+                        },
+                        {
+                            node: {
+                                title: "Baz",
+                            },
+                        },
+                    ],
+                },
             },
-            {
-              node: {
-                title: "Bar"
-              }
-            },
-            {
-              node: {
-                title: "Baz"
-              }
-            }
-          ]
-        }
-      }
-    };
+        };
 
-    return graphql(
-      `
-        {
-          repository(owner: "octokit", name: "graphql.js") {
-            issues(last: 3) {
-              edges {
-                node {
-                  title
+        return graphql(
+            `
+                {
+                    repository(owner: "octokit", name: "graphql.js") {
+                        issues(last: 3) {
+                            edges {
+                                node {
+                                    title
+                                }
+                            }
+                        }
+                    }
                 }
-              }
-            }
-          }
-        }
-      `,
-      {
-        headers: {
-          authorization: `token secret123`
-        },
-        request: {
-          fetch: fetchMock.sandbox().post(
-            "https://api.github.com/graphql",
-            { data: mockData },
+            `,
             {
-              headers: {
-                accept: "application/vnd.github.v3+json",
-                authorization: "token secret123",
-                "user-agent": userAgent
-              }
+                headers: {
+                    authorization: `token secret123`,
+                },
+                request: {
+                    fetch: fetchMock.sandbox().post(
+                        "https://api.github.com/graphql",
+                        { data: mockData },
+                        {
+                            headers: {
+                                accept: "application/vnd.github.v3+json",
+                                authorization: "token secret123",
+                                "user-agent": userAgent,
+                            },
+                        }
+                    ),
+                },
             }
-          )
-        }
-      }
-    ).then(result => {
-      expect(result).toStrictEqual(mockData);
+        ).then((result) => {
+            expect(result).toStrictEqual(mockData);
+        });
     });
-  });
 
-  it("Variables", () => {
-    const query = `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
+    it("Variables", () => {
+        const query = `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
       repository(owner:$owner, name:$repo) {
         issues(last:$num) {
           edges {
@@ -92,34 +89,29 @@ describe("graphql()", () => {
       }
     }`;
 
-    return graphql(query, {
-      headers: {
-        authorization: `token secret123`
-      },
-      owner: "octokit",
-      repo: "graphql.js",
-      request: {
-        fetch: fetchMock
-          .sandbox()
-          .post(
-            "https://api.github.com/graphql",
-            (url, options: RequestOptions) => {
-              const body = JSON.parse(options.body.toString());
-              expect(body.query).toEqual(query);
-              expect(body.variables).toStrictEqual({
-                owner: "octokit",
-                repo: "graphql.js"
-              });
+        return graphql(query, {
+            headers: {
+                authorization: `token secret123`,
+            },
+            owner: "octokit",
+            repo: "graphql.js",
+            request: {
+                fetch: fetchMock.sandbox().post("https://api.github.com/graphql", (url, options: OctokitTypes.RequestOptions) => {
+                    const body = JSON.parse(options.body);
+                    expect(body.query).toEqual(query);
+                    expect(body.variables).toStrictEqual({
+                        owner: "octokit",
+                        repo: "graphql.js",
+                    });
 
-              return { data: {} };
-            }
-          )
-      }
+                    return { data: {} };
+                }),
+            },
+        });
     });
-  });
 
-  it("Pass query together with headers and variables", () => {
-    const query = `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
+    it("Pass query together with headers and variables", () => {
+        const query = `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
       repository(owner:$owner, name:$repo) {
         issues(last:$num) {
           edges {
@@ -131,56 +123,66 @@ describe("graphql()", () => {
       }
     }`;
 
-    const options: RequestParameters = {
-      headers: {
-        authorization: `token secret123`
-      },
-      owner: "octokit",
-      query,
-      repo: "graphql.js",
-      request: {
-        fetch: fetchMock
-          .sandbox()
-          .post(
-            "https://api.github.com/graphql",
-            (url, options: RequestOptions) => {
-              const body = JSON.parse(options.body.toString());
-              expect(body.query).toEqual(query);
-              expect(body.variables).toStrictEqual({
-                owner: "octokit",
-                repo: "graphql.js"
-              });
+        const options: RequestParameters = {
+            headers: {
+                authorization: `token secret123`,
+            },
+            owner: "octokit",
+            query,
+            repo: "graphql.js",
+            request: {
+                fetch: fetchMock.sandbox().post("https://api.github.com/graphql", (url, options: OctokitTypes.RequestOptions) => {
+                    const body = JSON.parse(options.body);
+                    expect(body.query).toEqual(query);
+                    expect(body.variables).toStrictEqual({
+                        owner: "octokit",
+                        repo: "graphql.js",
+                    });
 
-              return { data: {} };
-            }
-          )
-      }
-    };
+                    return { data: {} };
+                }),
+            },
+        };
 
-    return graphql(options);
-  });
-
-  it("Don’t send empty variables object", () => {
-    const query = "{ viewer { login } }";
-
-    return graphql(query, {
-      headers: {
-        authorization: `token secret123`
-      },
-      request: {
-        fetch: fetchMock
-          .sandbox()
-          .post(
-            "https://api.github.com/graphql",
-            (url, options: RequestOptions) => {
-              const body = JSON.parse(options.body.toString());
-              expect(body.query).toEqual(query);
-              expect(body.variables).toEqual(undefined);
-
-              return { data: {} };
-            }
-          )
-      }
+        return graphql(options);
     });
-  });
+
+    it("Don’t send empty variables object", () => {
+        const query = "{ viewer { login } }";
+
+        return graphql(query, {
+            headers: {
+                authorization: `token secret123`,
+            },
+            request: {
+                fetch: fetchMock.sandbox().post("https://api.github.com/graphql", (url, options: OctokitTypes.RequestOptions) => {
+                    const body = JSON.parse(options.body);
+                    expect(body.query).toEqual(query);
+                    expect(body.variables).toEqual(undefined);
+
+                    return { data: {} };
+                }),
+            },
+        });
+    });
+
+    it("MediaType previews are added to header", () => {
+        const query = `{ viewer { login } }`;
+
+        return graphql(query, {
+            headers: {
+                authorization: `token secret123`,
+            },
+            owner: "octokit",
+            repo: "graphql.js",
+            mediaType: { previews: ["antiope", "testpkg"] },
+            request: {
+                fetch: fetchMock.sandbox().post("https://api.github.com/graphql", (url, options: OctokitTypes.RequestOptions) => {
+                    expect(options.headers.accept).toContain("antiope-preview");
+                    expect(options.headers.accept).toContain("testpkg-preview");
+                    return { data: {} };
+                }),
+            },
+        });
+    });
 });
