a4d43dba16c43e33c9ed2ae59a281bfdb89955c3
Chris Opperwall <copperwall@gmail.com>
2020-05-20 00:42:46 -0700

    feat(typescript): add generic support to graphql  (#99)
    This adds support for an optional generic to the graphql function
    exported by octokit/@graphql. If no type is passed, the return type with
    be an object  with  string keys and any values.

--- src/types.ts
+++ src/types.ts
@@ -20,7 +20,7 @@ export interface graphql {
    *
    * @param {object} endpoint Must set `method` and `url`. Plus URL, query or body parameters, as well as `headers`, `mediaType.{format|previews}`, `request`, or `baseUrl`.
    */
-  (options: RequestParameters): GraphQlResponse;
+  <ResponseData>(options: RequestParameters): GraphQlResponse<ResponseData>;
 
   /**
    * Sends a GraphQL query request based on endpoint options
@@ -28,7 +28,7 @@ export interface graphql {
    * @param {string} query GraphQL query. Example: `'query { viewer { login } }'`.
    * @param {object} [parameters] URL, query or body parameters, as well as `headers`, `mediaType.{format|previews}`, `request`, or `baseUrl`.
    */
-  (query: Query, parameters?: RequestParameters): GraphQlResponse;
+  <ResponseData>(query: Query, parameters?: RequestParameters): GraphQlResponse<ResponseData>;
 
   /**
    * Returns a new `endpoint` with updated route and parameters
@@ -43,14 +43,14 @@ export interface graphql {
 
 // export type withCustomRequest = (request: typeof Request) => graphql;
 
-export type GraphQlResponse = ReturnType<typeof graphql>;
+export type GraphQlResponse<ResponseData> = Promise<ResponseData>
 
 export type GraphQlQueryResponseData = {
   [key: string]: any;
-} | null;
+};
 
-export type GraphQlQueryResponse = {
-  data: GraphQlQueryResponseData;
+export type GraphQlQueryResponse<ResponseData> = {
+  data: ResponseData;
   errors?: [
     {
       message: string;
