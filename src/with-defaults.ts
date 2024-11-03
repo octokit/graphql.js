eee2a7dfbe29d27eeaf8d262f02909e68f1fdeff
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-11-03 12:48:20 +0200

    Add String & DocumentTypeDecoration overload

--- src/with-defaults.ts
+++ src/with-defaults.ts
@@ -5,6 +5,18 @@ import type {
   RequestParameters,
 } from "./types.js";
 import { graphql } from "./graphql.js";
+import type { DocumentTypeDecoration } from "@graphql-typed-document-node/core";
+
+/**
+ * Allows discarding the DocumentTypeDecoration information from the external
+ * interface, and casting the String part to string. This avoids the external
+ * API interface spreading to the internal types, while keeping type-safety for
+ * future extensions/changes.
+ */
+type DiscardTypeDecoration<T> = T extends String &
+  DocumentTypeDecoration<unknown, unknown>
+  ? string
+  : T;
 
 export function withDefaults(
   request: typeof Request,
@@ -12,10 +24,17 @@ export function withDefaults(
 ): ApiInterface {
   const newRequest = request.defaults(newDefaults);
   const newApi = <ResponseData>(
-    query: Query | RequestParameters,
+    query:
+      | Query
+      | (String & DocumentTypeDecoration<unknown, unknown>)
+      | RequestParameters,
     options?: RequestParameters,
   ) => {
-    return graphql<ResponseData>(newRequest, query, options);
+    return graphql<ResponseData>(
+      newRequest,
+      query as DiscardTypeDecoration<typeof query>,
+      options,
+    );
   };
 
   return Object.assign(newApi, {
